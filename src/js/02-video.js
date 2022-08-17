f1fa79ce1968d38db3f9dcf630dc4d363f0a42c3
vokur13 <vokur13@gmail.com>
2022-08-17 20:31:04 +0300

    task-02 added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import VimeoPlayer from '@vimeo/player';
+import LodashThrottle from 'lodash.throttle';
 
+// const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
